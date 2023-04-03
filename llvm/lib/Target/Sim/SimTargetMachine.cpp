a4dc9c0fa9ea42f76340ea0642fe648f84916cbc
derzhavin3016 <derzhavin.andrej@yandex.ru>
2023-04-04 00:19:12 +0300

    [Sim] Progress

--- llvm/lib/Target/Sim/SimTargetMachine.cpp
+++ llvm/lib/Target/Sim/SimTargetMachine.cpp
@@ -1,10 +1,3 @@
-//===-- SimTargetMachine.cpp - Define TargetMachine for Sim -------------===//
-//
-//                     The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
-//
 //===----------------------------------------------------------------------===//
 //
 // Implements the info about Sim target spec.
@@ -12,18 +5,87 @@
 //===----------------------------------------------------------------------===//
 
 #include "Sim.h"
-
+#include "SimTargetMachine.h"
+//#include "SimTargetTransformInfo.h"
+#include "TargetInfo/SimTargetInfo.h"
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
+#include "llvm/CodeGen/TargetPassConfig.h"
 #include "llvm/IR/Attributes.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/CodeGen.h"
-#include "llvm/CodeGen/Passes.h"
-#include "llvm/CodeGen/TargetPassConfig.h"
+#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/CodeGen.h"
+#include "llvm/Support/ErrorHandling.h"
 #include "llvm/Target/TargetOptions.h"
-
-using namespace llvm;
 
 #define DEBUG_TYPE "Sim"
 
-extern "C" void LLVMInitializeSimTarget() {
+using namespace llvm;
+
+static Reloc::Model getRelocModel(Optional<Reloc::Model> RM) {
+  return RM.getValueOr(Reloc::Static);
 }
+
+/// SimTargetMachine ctor - Create an ILP32 Architecture model
+SimTargetMachine::SimTargetMachine(const Target &T, const Triple &TT,
+                                   StringRef CPU, StringRef FS,
+                                   const TargetOptions &Options,
+                                   Optional<Reloc::Model> RM,
+                                   Optional<CodeModel::Model> CM,
+                                   CodeGenOpt::Level OL, bool JIT)
+    : LLVMTargetMachine(T,
+                        "e-m:e-p:32:32-i1:8:32-i8:8:32-i16:16:32-i32:32:32-"
+                        "f32:32:32-i64:32-f64:32-a:0:32-n32",
+                        TT, CPU, FS, Options, getRelocModel(RM),
+                        getEffectiveCodeModel(CM, CodeModel::Small), OL),
+      TLOF(std::make_unique<TargetLoweringObjectFileELF>()),
+      Subtarget(TT, std::string(CPU), std::string(FS), *this) {
+  initAsmInfo();
+}
+
+SimTargetMachine::~SimTargetMachine() = default;
+
+namespace {
+
+/// Sim Code Generator Pass Configuration Options.
+class SimPassConfig : public TargetPassConfig {
+public:
+  SimPassConfig(SimTargetMachine &TM, PassManagerBase &PM)
+      : TargetPassConfig(TM, PM) {}
+
+  SimTargetMachine &getSimTargetMachine() const {
+    return getTM<SimTargetMachine>();
+  }
+
+  bool addInstSelector() override;
+  // void addPreEmitPass() override;
+  // void addPreRegAlloc() override;
+};
+
+} // end anonymous namespace
+
+TargetPassConfig *SimTargetMachine::createPassConfig(PassManagerBase &PM) {
+  return new SimPassConfig(*this, PM);
+}
+
+bool SimPassConfig::addInstSelector() {
+  addPass(createSimISelDag(getSimTargetMachine(), getOptLevel()));
+  return false;
+}
+
+// void SimPassConfig::addPreEmitPass() { llvm_unreachable(""); }
+
+// void SimPassConfig::addPreRegAlloc() { llvm_unreachable(""); }
+
+// Force static initialization.
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeSimTarget() {
+  RegisterTargetMachine<SimTargetMachine> X(getTheSimTarget());
+}
+
+#if 0
+TargetTransformInfo
+SimTargetMachine::getTargetTransformInfo(const Function &F) {
+  return TargetTransformInfo(SimTTIImpl(this, F));
+}
+#endif
